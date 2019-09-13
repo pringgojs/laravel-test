7664e7ffc2d483909092e44750bf7d0cb05b2113
pringgojs <odyinggo@gmail.com>
2019-09-13 10:22:26 +0700

    hapus komentar

--- database/seeds/ClearCopyDatabaseTransaksi.php
+++ database/seeds/ClearCopyDatabaseTransaksi.php
@@ -23,34 +23,36 @@ class ClearCopyDatabaseTransaksi extends Seeder
      */
     public function run()
     {
+        ini_set('memory_limit', '-1');
+
         $list_tabel_transaksi = [
-            // 'transaksi_detail_usulan',
-            // 'transaksi_penilai_usulan',
-            // 'transaksi_penilai_usulan_copy',
-            // 'transaksi_usulan',
-            // 'transaksi_verifikator_penilai_usulan',
-            // 'transaksi_verifikator_penilai_usulan_distinct',
-            // 'usulan_perbulan',
-            // 'data_kti_guru',
-            // 'data_pak_guru',
-            // 'data_penilaian_final_kti_guru',
-            // 'data_penilaian_final_pak_guru',
-            // 'data_penilaian_kti_guru',
-            // 'data_surat_permintaan_kelengkapan',
+            'transaksi_detail_usulan',
+            'transaksi_penilai_usulan',
+            'transaksi_penilai_usulan_copy',
+            'transaksi_usulan',
+            'transaksi_verifikator_penilai_usulan',
+            'transaksi_verifikator_penilai_usulan_distinct',
+            'usulan_perbulan',
+            'data_kti_guru',
+            'data_pak_guru',
+            'data_penilaian_final_kti_guru',
+            'data_penilaian_final_pak_guru',
+            'data_penilaian_kti_guru',
+            'data_surat_permintaan_kelengkapan',
 
             'ref_jenis_pkb',
         ];
 
         \DB::beginTransaction();
         /** Perbaiki data */
-        // $pak_guru = DataPakGuru::where('periode_pangkat', '0000-00-00')
-        //     ->update(['periode_pangkat' => null]);
-        // $pak_guru = DataPakGuru::where('periode_jabatan', '0000-00-00')
-        //     ->update(['periode_jabatan' => null]);
-        // $pak_guru = DataPenilaianFinalPakGuru::where('periode_pangkat', '0000-00-00')
-        //     ->update(['periode_pangkat' => null]);
-        // $pak_guru = DataPenilaianFinalPakGuru::where('periode_jabatan', '0000-00-00')
-        //     ->update(['periode_jabatan' => null]);
+        $pak_guru = DataPakGuru::where('periode_pangkat', '0000-00-00')
+            ->update(['periode_pangkat' => null]);
+        $pak_guru = DataPakGuru::where('periode_jabatan', '0000-00-00')
+            ->update(['periode_jabatan' => null]);
+        $pak_guru = DataPenilaianFinalPakGuru::where('periode_pangkat', '0000-00-00')
+            ->update(['periode_pangkat' => null]);
+        $pak_guru = DataPenilaianFinalPakGuru::where('periode_jabatan', '0000-00-00')
+            ->update(['periode_jabatan' => null]);
 
         foreach ($list_tabel_transaksi as $tabel) {
             $new_tabel = $tabel.'_1';
